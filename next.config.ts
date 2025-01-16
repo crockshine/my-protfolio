21c2471ccaf43904052c43bbb072bba61d0d5b62
crockshine <mihail_korolev1111@mail.ru>
2025-01-16 21:15:46 +0300

    refactor: правка некст конфига

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "export",
-    basePath: "/my-portfolio",
-    images:{
-        unoptimized: true
-    }
   /* config options here */
 };
 
